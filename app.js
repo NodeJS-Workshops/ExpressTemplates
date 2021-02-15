a676da8054701a96f72a9b66f4ce28871f2a446b
TanyaRan <tatyanaran@gmail.com>
2021-02-15 17:13:47 +0200

    Set NODE_ENV, add PORT

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const config = require('./config/config');
 
 const app = express();
 
-console.log(process.env.NODE_ENV);
+// console.log(process.env.NODE_ENV);
 
-app.listen(5000);
+app.listen(config.PORT, () => {
+	console.log(`Server is running on PORT ${config.PORT}...`);
+});
