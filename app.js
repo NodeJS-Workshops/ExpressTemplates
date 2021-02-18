a4f9c0398ac6f9a866936450405cca85200e6479
TanyaRan <tatyanaran@gmail.com>
2021-02-18 17:04:53 +0200

    add view engine, add .hbs files in views folder

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const handlebars = require('express-handlebars');
 const config = require('./config/config');
 
 const app = express();
 
+app.engine(
+	'hbs',
+	handlebars({
+		extname: 'hbs'
+	})
+);
+app.set('view engine', 'hbs');
+
 // console.log(process.env.NODE_ENV);
+app.get('/', (req, res) => {
+	res.render('home');
+});
 
 app.listen(config.PORT, () => {
 	console.log(`Server is running on PORT ${config.PORT}...`);
