bea869579e3ceaf42219568a264ee1e2e9bb129f
TanyaRan <tatyanaran@gmail.com>
2021-03-21 15:59:13 +0200

    Extract express setup

--- app.js
+++ app.js
@@ -1,18 +1,9 @@
 const express = require('express');
-const handlebars = require('express-handlebars');
 const config = require('./config/config');
+const expressConfig = require('./config/express');
 
 const app = express();
-
-app.engine(
-  'hbs',
-  handlebars({
-    extname: 'hbs'
-  })
-);
-app.set('view engine', 'hbs');
-
-app.use(express.static('public'));
+expressConfig(app);
 
 // console.log(process.env.NODE_ENV);
 app.get('/', (req, res) => {
