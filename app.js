06eb8d85ff0317faa2992bca0b4af1a5c8f0fed9
TanyaRan <tatyanaran@gmail.com>
2021-03-22 13:10:06 +0200

    add routes

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const config = require('./config/config');
 const expressConfig = require('./config/express');
+const routes = require('./routes/cubeRoutes');
 
 const app = express();
 expressConfig(app);
 
-// console.log(process.env.NODE_ENV);
-app.get('/', (req, res) => {
-  res.render('home');
-});
+app.use(routes);
 
 app.listen(config.PORT, () => {
   console.log(`Server is running on PORT ${config.PORT}...`);
